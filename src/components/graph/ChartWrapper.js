c069e66f80288df15101f98f8e3c54ff9575f3fd
rai-roshan <kirito22037@gmail.com>
2022-01-12 11:25:51 +0530

    array with value , no index value , legend added for detail and limit the array length

--- src/components/graph/ChartWrapper.js
+++ src/components/graph/ChartWrapper.js
@@ -4,16 +4,40 @@ const Bar = ({ height }) => <div className={"bar"} style={{ height: `${height*5}
 
 </div>
 
+const ChartLegend = ({ array }) => {
+    let maxVal = array[0] ? array[0] : 0;
+    let minVal = array[0] ? array[0] : 0;
+
+    array.forEach( (value, index) => {
+       if( value > maxVal)
+           maxVal = value;
+       if( value < minVal)
+           minVal = value;
+    });
+
+    return <div id={"legend"}>
+        <span>height : value x 5 pix</span>
+        <span>array length : {array.length}</span>
+        <span>max value : {maxVal}</span>
+        <span>max height : {maxVal*5} pixel</span>
+        <span>min value : {minVal}</span>
+        <span>min height : {minVal*5} pixel</span>
+    </div>;
+}
+
 function randomLength( minLimit=10 ) {
-    return minLimit + Math.floor(Math.random() * 5000);
+    const length = Math.floor(Math.random() * 100);
+    console.log(length);
+    return length < minLimit ? minLimit : length;
 }
 
 function createRandomArray() {
     return [...Array(randomLength()+1).keys()].slice(1);
 }
 
-function  randomHeight( minLimit = 1) {
-    return minLimit + Math.floor(Math.random() * 100);
+function  randomHeight( minLimit = 0) {
+    const height = Math.floor(Math.random() * 100);
+    return height < minLimit ? minLimit : height;
 }
 
 const ChartWrapper = () => {
@@ -24,13 +48,17 @@ const ChartWrapper = () => {
     }
 
     return <div id={"chart-wrapper"} className={"flex-xy-center"}>
-        { array.map( (value, index) => (
-            <Bar key={`bar-no-${index}`} height={randomHeight()} />
-        ) )
+        { array.map( (value, index) => {
+            array[index] =  randomHeight();
+            return <Bar
+                key={`bar-no-${index}`}
+                height={ array[index] } />
+        } )
         }
         <button className={'debug-btn'} onClick={ generateRandomGraph }>
-            random array
+            refresh
         </button>
+        <ChartLegend array={array} />
     </div>
 };
 
